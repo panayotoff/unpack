985c2d29a706591ea83624ca61ea0e90a044eddc
Chris <burnandbass@gmail.com>
2019-02-21 17:06:23 +0200

    Add File Browser

--- devtools-page/index.js
+++ devtools-page/index.js
@@ -1,20 +1,29 @@
 //http://qaru.site/questions/7842022/chromedownloadsdownload-api-saveas-dialogue-is-flashing-on-the-screen-and-then-closes-before-i-see-the-dialogue
 
-// document.querySelector('#fetch-all').addEventListener('click', () => {
-//   chrome.devtools.inspectedWindow.getResources(resources => {
-//     const webpackResources = resources.filter(resource => resource.url.startsWith('webpack://'));
-//     processWebpackResources(webpackResources);
-//   });
-// });
-
+// Reloads page
 document.querySelector('#reload-page').addEventListener('click', () => {
   chrome.tabs.reload(chrome.devtools.inspectedWindow.tabId, null, () => {});
 });
 
+// Fetch all resources
+document.querySelector('#fetch').addEventListener('click', async () => {
+  const webpackResources = await getWindowWebpackResources();
+  const resourcesTree = flatToTree(webpackResources.src);
+  const fileExplorerDom = treeToDom(resourcesTree);
+  document.querySelector('#file-explorer').innerHTML = fileExplorerDom;
+  document.querySelector('#file-explorer').addEventListener('click', async event => {
+    if (event.target.matches('.file')) {
+      const filePath = event.target.getAttribute('data-file');
+      const file = webpackResources.src.find(r => r.path === filePath);
+      const fileContent = await file.content;
+    }
+  });
+});
+
+// Download all resources
 document.querySelector('#download-all').addEventListener('click', () => {
   chrome.devtools.inspectedWindow.getResources(async resources => {
-    const webpackResources = resources.filter(resource => resource.url.startsWith('webpack://'));
-    const resourcesMap = await getResourcesMap(webpackResources);
+    const webpackResources = await getWindowWebpackResources();
 
     if (JSZip) {
       chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
@@ -23,10 +32,10 @@ document.querySelector('#download-all').addEventListener('click', () => {
         const zip = new JSZip();
 
         let node_modules_file_contents = '';
-        resourcesMap.node_modules.forEach(module => (node_modules_file_contents += `${module}\n`));
+        webpackResources.node_modules.forEach(module => (node_modules_file_contents += `${module}\n`));
 
         zip.folder(domain).file('node_modules.md', node_modules_file_contents);
-        resourcesMap.src.forEach(resource => {
+        webpackResources.src.forEach(resource => {
           zip.file(`${domain}/src/${resource.path}`, resource.content);
         });
 
@@ -38,8 +47,12 @@ document.querySelector('#download-all').addEventListener('click', () => {
   });
 });
 
+/**
+ *
+ * @param {Blob} content
+ * @param {String} filename
+ */
 function downloadZip(content, filename) {
-  // const dataURL = 'data:application/zip;base64,' + content;
   const url = URL.createObjectURL(content);
   chrome.downloads.download({
     url: url,
@@ -48,51 +61,124 @@ function downloadZip(content, filename) {
   });
 }
 
-async function processWebpackResources(resources) {
-  const resourcesMap = await getResourcesMap(resources);
-  console.log('resouces map', resourcesMap);
-}
+/**
+ *  All Webpack resources from current window
+ */
+function getWindowWebpackResources() {
+  return new Promise(resolve => {
+    chrome.devtools.inspectedWindow.getResources(windowResources => {
+      const webpackResources = windowResources.filter(resource => resource.url.startsWith('webpack://'));
 
-// Builds map with all node_modules and resourxes
-function getResourcesMap(resources) {
-  const resourcesMap = {
-    node_modules: new Set(),
-    src: []
-  };
-  resources.forEach(async resource => {
-    console.log(resource.url);
+      const resources = {
+        node_modules: new Set(),
+        src: []
+      };
 
-    if (resource.url.includes('node_modules')) {
-      const module_name = resource.url.split('node_modules/')[1].split('/')[0];
-      resourcesMap.node_modules.add(module_name);
-    } else {
-      let resource_path;
-      if (resource.url.includes('src/')) {
-        resource_path = resource.url.split('src/')[1];
-      }
-      if (resource.url.startsWith('webpack:///.')) {
-        resource_path = resource.url.split('webpack:///.')[1];
-      }
+      webpackResources.forEach((resource, index) => {
+        if (resource.url.includes('node_modules')) {
+          const module_name = resource.url.split('node_modules/')[1].split('/')[0]; // TODO: @vue/ ; @babel/
+          resources.node_modules.add(module_name);
+        } else {
+          const path = normalizePath(resource.url);
+          const content = getContent(resource);
 
-      const resource_content = await getContent(resource);
-      const resource_dirname = resource_path.split('/');
-      const resource_filename = resource_dirname.pop();
-      resourcesMap.src.push({
-        path: resource_path,
-        dirname: resource_dirname,
-        filename: resource_filename,
-        content: resource_content
+          if (path) {
+            resources.src.push({
+              path,
+              content
+            });
+          }
+        }
       });
-    }
+
+      // Wait for all resouces to be loaded....
+      Promise.all(resources.src.map(s => s.content)).then(() => {
+        resolve(resources);
+      });
+    });
   });
-  return resourcesMap;
 }
 
-// Helper - async getContent
+/**
+ * Async helper to get resource content
+ * @param {ChomeResource} resource
+ */
 function getContent(resource) {
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
     resource.getContent(content => {
       resolve(content);
     });
   });
 }
+
+/**
+ * Normalize webpack://url to what supposed to be original filesystem path
+ * @param {Url} path
+ */
+function normalizePath(path) {
+  if (path.includes('src/')) {
+    return path.split('src/')[1];
+  }
+  if (path.startsWith('webpack:///.')) {
+    return path.split('webpack:///.')[1];
+  }
+
+  if (path.startsWith('webpack:///./src')) {
+    return path.split('webpack:///./src')[1];
+  }
+
+  if (path.startsWith('webpack:///./')) {
+    return path.split('webpack:///./')[1];
+  }
+}
+
+// Helpers
+const flatToTree = data => {
+  const tree = { name: 'Webpack', type: 'folder', nodes: [] };
+  data.forEach(file => {
+    const segments = file.path.split('/');
+    let path = tree;
+    while (segments.length) {
+      const segment = segments.shift();
+      if (segments.length === 0) {
+        path.nodes.push({ type: 'file', name: segment, file: file.path });
+      } else {
+        let newPath = path.nodes.find(node => node.name === segment);
+        if (!newPath) {
+          newPath = { name: segment, type: 'folder', nodes: [] };
+          path.nodes.push(newPath);
+        }
+        path = newPath;
+      }
+    }
+  });
+  return tree;
+};
+
+/**
+ * Tree data to dom
+ * @param {*} data
+ */
+const treeToDom = data => {
+  let uniqid = 0;
+
+  function nodeMarkup(node) {
+    if (node.nodes) {
+      let str = `<li class="folder">
+        <input id="f${uniqid}" type="checkbox" />
+        <label for="f${uniqid}"><span>${node.name}</span></label>
+        <ul class="folder-content">`;
+      node.nodes.forEach(n => {
+        uniqid++;
+        str += nodeMarkup(n, str);
+      });
+      str += `</ul></li>`;
+      return str;
+    }
+
+    // Single node
+    return `<li class="file" data-file="${node.file}">${node.name}</li>`;
+  }
+
+  return `<ul class="root">${nodeMarkup(data)}</ul>`;
+};
